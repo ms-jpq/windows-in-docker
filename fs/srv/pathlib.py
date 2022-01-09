cefcb0a153a6fe8fe48fddb5a4ea5836eb9cdce5
IIIIII <github@bigly.dog>
2022-01-09 10:24:21 -0800

    ok

--- fs/srv/pathlib.py
+++ fs/srv/pathlib.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from stat import S_IRGRP, S_IROTH, S_IRUSR, S_IWUSR, S_IXGRP, S_IXOTH, S_IXUSR
 
-FOLDER_MODE = (S_IRUSR | S_IWUSR | S_IXUSR) | (S_IRGRP | S_IXGRP) | (S_IROTH | S_IXOTH)
-FILE_MODE = (S_IRUSR | S_IWUSR) | (S_IRGRP) | (S_IROTH)
+RWXR_XR_X = (S_IRUSR | S_IWUSR | S_IXUSR) | (S_IRGRP | S_IXGRP) | (S_IROTH | S_IXOTH)
+RW_R__R__ = (S_IRUSR | S_IWUSR) | (S_IRGRP) | (S_IROTH)
 
 
 def chmod(path: Path) -> None:
-    path.chmod(FOLDER_MODE if path.is_dir() else FILE_MODE)
+    path.chmod(RWXR_XR_X if path.is_dir() else RW_R__R__)
